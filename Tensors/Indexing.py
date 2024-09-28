28e614d3715ea90aba32faca09c1171470f28834
Gaurav Sharma <gsharma1154@gmail.com>
2024-09-28 23:45:25 +0530

    Indexing done in tensor

---
+++ Tensors/Indexing.py
@@ -0,0 +1,9 @@
+import torch
+
+#Indexing
+x=torch.tensor([[1,2],[3,4],[5,6],[7,8]])
+print(x)  #returns a tensor
+print(x[1,1])  #indexing also returns a tensor
+#.item() method returns a value as a python number
+print(x[1,1].item())
+
